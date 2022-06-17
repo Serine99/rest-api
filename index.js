ee2a5bc49c583ed1e3b4060723f4efa5210caeda
Serine99 <serine.sardaryan1@gmail.com>
2022-06-17 23:13:20 +0400

    rest api

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use("/users", usersRoutes);
+
+app.get("/", (req, res) => {
+  console.log("test");
+  res.send("Hello");
+});
+
+app.listen(port, () =>
+  console.log(`Server is running on port: http://localhost:${port}`)
+);
---
+++ routes/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  CreateUserController,
+  getUserController,
+  deleteUserController,
+  updateUserController,
+  getUsersController,
+} from "../controllers/control.js";
+
+const router = express.Router();
+
+// All routes in here are starting with /users
+router.get("/", getUsersController);
+
+router.post("/", CreateUserController);
+
+router.get("/:id", getUserController);
+
+router.delete("/:id", deleteUserController);
+
+router.put("/:id", updateUserController);
+
+export default router;
